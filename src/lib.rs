5f2c79d05011fe9ed3a22da4fb5146558b7b37d1
aashi-ihsaa <96407263+aashi-ihsaa@users.noreply.github.com>
2023-07-30 23:59:44 +0530

    chenge

--- src/lib.rs
+++ src/lib.rs
@@ -12,54 +12,17 @@ pub async fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn handler(headers: Vec<(String, String)>, qry: HashMap<String, Value>, body: Vec<u8>) {
+async fn handler(headers: Vec<(String, String)>, qry: HashMap<String, Value>, _body: Vec<u8>) {
     logger::init();
     log::info!("Headers -- {:?}", headers);
 
-    let username = match qry.get("username") {
-        Some(username_value) => {
-            match username_value.as_str() {
-                Some(username) => username,
-                None => {
-                    send_response(400, vec![], b"Bad Request: Invalid 'username' parameter.".to_vec());
-                    return;
-                }
-            }
-        }
-        None => {
-            send_response(400, vec![], b"Bad Request: Missing 'username' parameter.".to_vec());
-            return;
-        }
-    };
-
-    let password = match qry.get("password") {
-        Some(password_value) => {
-            match password_value.as_str() {
-                Some(password) => password,
-                None => {
-                    send_response(400, vec![], b"Bad Request: Invalid 'password' parameter.".to_vec());
-                    return;
-                }
-            }
-        }
-        None => {
-            send_response(400, vec![], b"Bad Request: Missing 'password' parameter.".to_vec());
-            return;
-        }
-    };
-
-    
-    let login_success = username == "admin" && password == "admin";
-
-    let resp = if login_success {
-        "Login Successful!".to_string()
-    } else {
-        "Login Failed: Invalid credentials.".to_string()
-    };
+    let msg = qry.get("msg").unwrap();
+    // let msg = String::from_utf8(body).unwrap_or("".to_string());
+    let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg);
 
     send_response(
         200,
-        vec![(String::from("content-type"), String::from("text/plain"))],
+        vec![(String::from("content-type"), String::from("text/html"))],
         resp.as_bytes().to_vec(),
     );
 }
